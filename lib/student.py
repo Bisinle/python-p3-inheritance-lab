736a06f2633fc548da134cbd51591bbf1cc5cfe4
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-22 23:26:35 +0300

    passed all the tests

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,24 @@
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
+
+    def learn(self, unit):
+        if unit not in self.knowledge:
+            self.knowledge.append(unit)
+
+
+# knowledge = [
+#     "str is a data type in Python",
+# ]
+# s = Student("Halani", "hikey", knowledge)
+# s.learn(
+#     "JavaScript async web request",
+# )
